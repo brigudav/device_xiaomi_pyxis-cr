5f3384b1a7c39ce27d93d4c3fe4eeda7312844df
brigudav <brigudav@gmail.com>
2020-10-13 14:29:35 +1000

    init

---
+++ fod/FingerprintInscreen.cpp
@@ -0,0 +1,205 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "FingerprintInscreenService"
+
+#include "FingerprintInscreen.h"
+
+#include <android-base/logging.h>
+#include <hardware_legacy/power.h>
+
+#include <fstream>
+#include <cmath>
+
+#define FINGERPRINT_ACQUIRED_VENDOR 6
+
+#define COMMAND_NIT 10
+#define PARAM_NIT_630_FOD 1
+#define PARAM_NIT_NONE 0
+
+#define DISPPARAM_PATH "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/disp_param"
+#define DISPPARAM_HBM_FOD_ON "0x20000"
+#define DISPPARAM_HBM_FOD_OFF "0xE0000"
+
+#define FOD_STATUS_PATH "/sys/devices/virtual/touch/tp_dev/fod_status"
+#define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF 0
+
+#define HBM_OFF_DELAY 50
+#define HBM_ON_DELAY 250
+
+namespace vendor {
+namespace lineage {
+namespace biometrics {
+namespace fingerprint {
+namespace inscreen {
+namespace V1_1 {
+namespace implementation {
+
+template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+template <typename T>
+static void set(const std::string& path, const T& value) {
+    std::ofstream file(path);
+    file << value;
+}
+
+FingerprintInscreen::FingerprintInscreen() {
+    xiaomiFingerprintService = IXiaomiFingerprint::getService();
+}
+
+Return<int32_t> FingerprintInscreen::getPositionX() {
+    return 455;
+}
+
+Return<int32_t> FingerprintInscreen::getPositionY() {
+    return 1910;
+}
+
+Return<int32_t> FingerprintInscreen::getSize() {
+    return 190;
+}
+
+Return<void> FingerprintInscreen::onStartEnroll() {
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onFinishEnroll() {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getHbmOffDelay() {
+    return HBM_OFF_DELAY;
+}
+
+Return<int32_t> FingerprintInscreen::getHbmOnDelay() {
+    return HBM_ON_DELAY;
+}
+
+Return<bool> FingerprintInscreen::supportsAlwaysOnHBM() {
+    return true;
+}
+
+Return<void> FingerprintInscreen::switchHbm(bool enabled) {
+    if (enabled) {
+        set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_ON);
+    } else {
+        set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
+    }
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onPress() {
+    acquire_wake_lock(PARTIAL_WAKE_LOCK, LOG_TAG);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_630_FOD);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onRelease() {
+    release_wake_lock(LOG_TAG);
+    xiaomiFingerprintService->extCmd(COMMAND_NIT, PARAM_NIT_NONE);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onShowFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_ON);
+    return Void();
+}
+
+Return<void> FingerprintInscreen::onHideFODView() {
+    set(FOD_STATUS_PATH, FOD_STATUS_OFF);
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::handleAcquired(int32_t acquiredInfo, int32_t vendorCode) {
+    std::lock_guard<std::mutex> _lock(mCallbackLock);
+    if (mCallback == nullptr) {
+        return false;
+    }
+
+    if (acquiredInfo == FINGERPRINT_ACQUIRED_VENDOR) {
+        if (vendorCode == 0) {
+            Return<void> ret = mCallback->onFingerDown();
+            if (!ret.isOk()) {
+                LOG(ERROR) << "FingerDown() error: " << ret.description();
+            }
+            return true;
+        }
+
+        if (vendorCode == 1) {
+            Return<void> ret = mCallback->onFingerUp();
+            if (!ret.isOk()) {
+                LOG(ERROR) << "FingerUp() error: " << ret.description();
+            }
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Return<bool> FingerprintInscreen::handleError(int32_t error, int32_t vendorCode) {
+    LOG(ERROR) << "error: " << error << ", vendorCode: " << vendorCode << "\n";
+    return false;
+}
+
+Return<void> FingerprintInscreen::setLongPressEnabled(bool) {
+    return Void();
+}
+
+Return<int32_t> FingerprintInscreen::getDimAmount(int32_t brightness) {
+    float alpha;
+    int realBrightness = brightness * 2047 / 255;
+
+    if (realBrightness > 500) {
+        alpha = 1.0 - pow(realBrightness / 2047.0 * 430.0 / 600.0, 0.455);
+    } else {
+        alpha = 1.0 - pow(realBrightness / 1680.0, 0.455);
+    }
+
+    return 255 * alpha;
+}
+
+Return<bool> FingerprintInscreen::shouldBoostBrightness() {
+    return false;
+}
+
+Return<void> FingerprintInscreen::setCallback(const sp<IFingerprintInscreenCallback>& callback) {
+    {
+        std::lock_guard<std::mutex> _lock(mCallbackLock);
+        mCallback = callback;
+    }
+
+    return Void();
+}
+
+Return<bool> FingerprintInscreen::noDim() {
+    return false;
+}
+
+}  // namespace implementation
+}  // namespace V1_1
+}  // namespace inscreen
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace lineage
+}  // namespace vendor
